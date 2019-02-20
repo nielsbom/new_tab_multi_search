521bdffcbe4e70aafff8d94cbafd5bffb63d55d8
Niels Bom <niels@nielsbom.com>
2019-02-20 17:17:35 +0100

    Add files via upload

--- work/js/index.js
+++ work/js/index.js
@@ -1,1 +1,10 @@
-
+$( document ).ready(function() {
+    $("form").submit(function(event) {
+      var url = $(this).data("url");
+      var searchterm = $(this).find('input[name="searchterm"]').val();
+      
+      url = url.replace("{query}", searchterm);
+      window.open(url, '_blank')
+      event.preventDefault();
+    })
+});
